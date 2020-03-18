7f3db4ede701ca4a3a9b97a83f80f6fbfe56f172
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-18 15:37:35 -0500

    [Feature] Created file to manage global variables

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '18363101088-84jr9h2lecks85hilkmddpm14orarffb.apps.googleusercontent.com',
+  googleClientSecret: 'e6oAkjyiT456Vy_xDN9MjBuf',
+  mongoURI: 'mongodb+srv://dbUser:password4856@emailly-dev-pcyph.mongodb.net/emaily-dev?retryWrites=true&w=majority'
+}
