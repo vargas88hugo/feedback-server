18b4bbffdb1e59f86e6e10af282b06db5673e595
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-18 16:05:22 -0500

    [Update] added cookie key

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleClientID: '18363101088-84jr9h2lecks85hilkmddpm14orarffb.apps.googleusercontent.com',
   googleClientSecret: 'e6oAkjyiT456Vy_xDN9MjBuf',
-  mongoURI: 'mongodb+srv://dbUser:password4856@emailly-dev-pcyph.mongodb.net/emaily-dev?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://dbUser:password4856@emailly-dev-pcyph.mongodb.net/emaily-dev?retryWrites=true&w=majority',
+  cookieKey: 'asdasduuhuhi9pl,mnkbh'
 }
