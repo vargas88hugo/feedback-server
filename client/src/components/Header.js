63d7739f6d56f6054bee43b513c57e749c96c143
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-22 17:44:38 -0500

    [Update] Configured login and logout routes with full http request

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+  
   render() { 
     return (
       <nav>
         <div className="nav-wrapper grey darken-1">
           <a className="left brand-logo" href="/">Logo</a>
           <ul className="right">
-            <li>
-              <a href="/">Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     );
   }
 }
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
  
-export default Header;
+export default connect(mapStateToProps)(Header);
