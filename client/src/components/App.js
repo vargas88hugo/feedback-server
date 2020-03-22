fedfab703a5902207f9cc59e3dac463bf0e40be5
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-22 16:01:58 -0500

    [Feature] Added redux thunk and tested with console log

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Header></Header>
-          <Route exact path="/" component={Landing}></Route>
-          <Route exact path="/surveys" component={Dashboard}></Route>
-          <Route path="/surveys/new" component={SurveyNew}></Route>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header></Header>
+            <Route exact path="/" component={Landing}></Route>
+            <Route exact path="/surveys" component={Dashboard}></Route>
+            <Route path="/surveys/new" component={SurveyNew}></Route>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
