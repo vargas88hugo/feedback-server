99c469713eec07ee5fc31690852cf07370c91b9c
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-23 14:19:56 -0500

    [Feature] Added stripe service to payment processing, and works with test mode

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as actions from '../actions';
 
+import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 const Dashboard = () => <h2>Dashboard</h2>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,24 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends Component {
   renderContent() {
-    console.log(this.props);
     switch (this.props.auth) {
       case null:
         return;
       case false:
-        return (
-          <li>
-            <a href="/auth/google">Login with Google</a>
-          </li>
-        );
+        return <li><a href="/auth/google">Login with Google</a></li>;
       default:
-        return (
-          <li>
-            <a href="/api/logout">Logout</a>
-          </li>
-        );
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
   
