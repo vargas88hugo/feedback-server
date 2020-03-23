6a8f0cc48f433897bbafbbef112fd992d19e539b
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-22 21:34:35 -0500

    [Feature] Changed key system and configured icon router of header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
+    console.log(this.props);
     switch (this.props.auth) {
       case null:
         return;
@@ -25,7 +27,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper grey darken-1">
-          <a className="left brand-logo" href="/">Logo</a>
+          <Link 
+            to={this.props.auth ? '/surveys' : '/'} 
+            className="left brand-logo"
+          >
+            Logo
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  googleClientID: '18363101088-84jr9h2lecks85hilkmddpm14orarffb.apps.googleusercontent.com',
-  googleClientSecret: 'e6oAkjyiT456Vy_xDN9MjBuf',
-  mongoURI: 'mongodb+srv://dbUser:password4856@emailly-dev-pcyph.mongodb.net/emaily-dev?retryWrites=true&w=majority',
-  cookieKey: 'asdasduuhuhi9pl,mnkbh'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
 }
