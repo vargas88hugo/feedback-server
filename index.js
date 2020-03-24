b575abde22ec3c563bbcb9cae42c74f34953bebb
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-23 20:41:47 -0500

    [Update] Implemented auto routing to react in production mode

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const properties = require('./config/properties');
 const authRoutes = require('./routes/authRoutes');
@@ -26,6 +27,14 @@ app.use(passport.session());
 
 authRoutes(app);
 billingRoutes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 app.listen(properties.PORT, () => {
   console.log(`Server running on port ${properties.PORT}`);
