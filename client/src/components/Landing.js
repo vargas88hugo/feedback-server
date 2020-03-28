721f43d89aa3200dbd657700ff2411778381b409
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-27 22:58:22 -0500

    [Update] Added instructions in the home page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
     <div style={{ textAlign: 'center' }}>
       <h2>Welcome to the Surveys Feedback!</h2>
       <h5>
-        This app can send emails for people to vote surveys.
+        This app can send multiple emails for people to vote surveys.
       </h5>
       <h5>
         If you want to prove this service. Please enter this credit card number for credits!
@@ -13,6 +13,12 @@ const Landing = () => {
       <h3>
         4242 4242 4242 4242
       </h3>
+      <h6>
+        Updating the votes may take a few minutes
+      </h6>
+      <h6>
+        To send multiple emails you must separate them by commas
+      </h6>
     </div>
   );
 };
